8c7931226bba9467306c675f26b441705a1141de
EDZephyr <103563317+EDZephyr@users.noreply.github.com>
2024-10-22 21:15:59 +0900

    lnkstructure

--- lnkStructure.py
+++ lnkStructure.py
@@ -4,63 +4,41 @@ def hextoint(hex):
     return int(hex, 16)
 
 def extract_extrablock_ID(lnk_hex_string):
-    id_list= []
-
-    flags= calc_little_endian_bit(lnk_hex_string[40:49])
-
-    current_number= 0
-    header_size= hextoint("4C")
-
-    current_number += header_size * 2
-
-    if (flags[0] == "1"):
-        jump_value= hextoint(calc_little_endian_byte(lnk_hex_string[current_number:current_number+4]))* 2
-        current_number+= 4
-        current_number+= jump_value
-
-    if (flags[1] == "1"):
-        jump_value= hextoint(calc_little_endian_byte(lnk_hex_string[current_number:current_number+8]))* 2
-        current_number+= 8
-        current_number+= jump_value
-
-    if (flags[2] == "1"):
-        print(current_number)
-        jump_value= hextoint(calc_little_endian_byte(lnk_hex_string[current_number:current_number+4]))* 2
-        current_number+= 4
-        current_number+= jump_value
-        print(current_number)
-
-    if (flags[3] == "1"):
-        jump_value= hextoint(calc_little_endian_byte(lnk_hex_string[current_number:current_number+4]))* 2
-        current_number+= 4
-        current_number+= jump_value
-
-    if (flags[4] == "1"):
-        jump_value= hextoint(calc_little_endian_byte(lnk_hex_string[current_number:current_number+4]))* 2
-        current_number+= 4
-        current_number+= jump_value
-
-    if (flags[5] == "1"):
-        jump_value= hextoint(calc_little_endian_byte(lnk_hex_string[current_number:current_number+4]))* 2
-        current_number+= 4
-        current_number+= jump_value
-        print(jump_value)
-        print(current_number)
-
-    if (flags[6] == "1"):
-        jump_value= hextoint(calc_little_endian_byte(lnk_hex_string[current_number:current_number+4]))* 2
-        current_number+= 4
-        current_number+= jump_value
-
-
-
-    print (current_number)
-    while(current_number< len(lnk_hex_string)-1):
-        if ((lnk_hex_string[current_number+9:current_number+17]) == "00000000"):
+    id_list = []
+    
+    current_position = 76 * 2
+    
+    flags = calc_little_endian_bit(lnk_hex_string[40:48])
+    
+    if flags[0] == "1":
+        list_size = hextoint(calc_little_endian_byte(lnk_hex_string[current_position:current_position+4]))
+        current_position += 4 * 2 
+        current_position += list_size * 2
+    
+    if flags[1] == "1":
+        info_size = hextoint(calc_little_endian_byte(lnk_hex_string[current_position:current_position+4]))
+        current_position += info_size * 2
+    
+    for i in range(2, 7):
+        if flags[i] == "1":
+            count = hextoint(calc_little_endian_byte(lnk_hex_string[current_position:current_position+2]))
+            current_position += 2 * 2
+            
+            current_position += count * 2 * 2
+    
+    while current_position < len(lnk_hex_string) - 16:
+        block_size = hextoint(calc_little_endian_byte(lnk_hex_string[current_position:current_position+8]))
+        
+        if block_size == 0:
             break
-        id_list.append(lnk_hex_string[current_number+9:current_number+17])
-        print(lnk_hex_string[current_number+9:current_number+17])
-        jump_value= hextoint(calc_little_endian_byte(lnk_hex_string[current_number+1:current_number+9]))* 2
-        current_number+= jump_value
-
+            
+        signature = lnk_hex_string[current_position+8:current_position+16]
+        
+        id_list.append(signature)
+        
+        current_position += block_size * 2
+        
+        if block_size < 8:
+            break
+            
     return id_list
